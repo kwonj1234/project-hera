add2efcf7ce7ad89f1dbedb242e3727f019d3d32
kwonj1234 <kwonj1234@gmail.com>
2024-03-05 00:59:09 -0600

    add auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,20 @@
-import { PaperProvider } from 'react-native-paper';
+import { ActivityIndicator, PaperProvider } from 'react-native-paper';
+
+import { AuthProvider, useAuth } from '@/src/providers';
 import Router from '@/src/router'
 
 export default function App() {
+  const { session, loading } = useAuth()
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
   return (
     <PaperProvider>
+      <AuthProvider>
         <Router />
+      </AuthProvider>
     </PaperProvider>
   );
 }
